783d8f5741706b52e62b97f13ab052cc23b18774
Marie Hazel Anderson <marieanderson@138-38-180-215.eduroam.bath.ac.uk>
2019-03-18 19:44:47 +0000

    basic hello website

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask("MyApp")
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data = request.form
+    email = form_data["email"]
+    send_simple_message(email)
+    return "All OK"
+
+def send_simple_message(address):
+	return requests.post(
+		"https://api.mailgun.net/v3/sandboxb298abe6bf9b4f2597ff936c91181223.mailgun.org/messages",
+		auth=("api", "dc8d31d20b0995dd1e58bfc3dc650cd6-de7062c6-b92fa5a7"),
+		data={"from": "Wazzappp <mailgun@sandboxb298abe6bf9b4f2597ff936c91181223.mailgun.org>",
+			"to": [address],
+			"subject": "What up homie?",
+			"text": "How's it hangin'?"})
+
+
+app.run(debug=True)
+app.run(port=5050)
