19489277fca22f6a8df63ffaa193a5098bdb5e97
MH18Anderson <47473865+MH18Anderson@users.noreply.github.com>
2019-03-31 23:07:09 +0100

    obtain recent news articles

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ def sign_up():
     name = form_data["username"]
     course = form_data["course"]
     send_simple_message(email)
-    return render_template("hello.html", name=name.title())
+    articles = find_news()
+    return render_template("hello.html", name=name.title(), articles=articles)
 
+# sends a welcome email to users when they login
 def send_simple_message(address):
 	return requests.post(
 		"https://api.mailgun.net/v3/sandboxb298abe6bf9b4f2597ff936c91181223.mailgun.org/messages",
@@ -24,6 +26,26 @@ def send_simple_message(address):
 			"to": [address],
 			"subject": "University made easier ;)",
 			"text": "Thanks for signing up!"})
+
+
+# will request the news articles 'Powered by NewsApi'
+@app.route('/', methods=['GET'])
+@cross_origin()
+def find_news():
+    newsapi = NewsApiClient(api_key='becec0b66fa741fb9868408992b4587a')
+
+    # get all articles relating to users' course
+    all_articles = newsapi.get_everything(q='brexit',
+                                         sources='bbc-news, bloomberg, business-insider-uk, financial-times',
+                                         domains='bbc.co.uk, bloomberg.com, uk.businessinsider.com, ft.com',
+                                         from_param='2019-03-01',
+                                         to='2019-03-20',
+                                         language='en',
+                                         sort_by='relevancy',
+                                         page=2)
+    return jsonify(all_articles)
+
+
 
 
 app.run(debug=True)
